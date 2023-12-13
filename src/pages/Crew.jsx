3c9525b4f9a7d0ff044bc0cbf5fec00be495a235
Emmanuel <barretosantiago96@hotmail.com>
2023-12-13 12:14:00 -0300

    crew terminado css +/-

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -4,28 +4,24 @@ export const Crew = () => {
     <>
       <section
         className="bg-crew-mobile 
-                   md:bg-crew-tablet
-                   lg:bg-crew-desktop
                    bg-cover 
                    bg-no-repeat 
-                   min-h-screen"
+                   text-center
+                   min-h-screen
+                   px-20
+                   pt-40
+                  text-White
+                   md:bg-crew-tablet
+                   lg:text-start
+                   lg:bg-crew-desktop"
       >
-        <div
-          className="flex
-                     flex-col
-                     items-center 
-                     px-6 
-                     text-White"
-        >
-          <h2
+        <h2
             className="font-Barlow
                        tracking-[2.7px]
                        w-full
-                       mt-28
-                       md:mt-36
                        mb-10
-                       text-center
-                       md:text-start"
+                       lg:text-3xl
+                       "
           >
             <span
               className="font-bold 
@@ -36,37 +32,96 @@ export const Crew = () => {
             </span>
             MEET YOUR CREW
           </h2>
-          <div className="order-2 md:order-1 font-bellefair text-center text-Azul-Claro">
-            <h3>COMMANDER</h3>
-            <h1 className="text-2xl text-White mt-2 mb-7">DOUGLAS HURLEY</h1>
-            <p className="font-barlow mb-10">
+        <div
+          className="flex
+                     flex-col
+                     items-center
+                     lg:
+                     lg:items-star
+                     lg:w-[50%]
+                     xl:w-[40%]"
+        >
+          <img
+            className="h-52 
+                     border-White 
+                       border-b 
+                       md:border-0 
+                       md:h-[420px] 
+                       md:mt-2 
+                       md:order-3 
+                       lg:h-[75%] 
+                       lg:order-2 
+                       lg:absolute 
+                       lg:bottom-0 
+                       lg:right-[10%]"
+            src={people}
+            alt=""
+          />
+          <div 
+            className="flex 
+                       gap-4 
+                       my-10
+                       md:order-2  
+                       lg:justify-center 
+                       lg:order-3 
+                       lg:w-full"
+            >
+            <button
+              type="button"
+              className="rounded-full
+                      bg-White 
+                         w-[10px] 
+                         h-[10px]"
+            ></button>
+            <button
+              type="button"
+              className="rounded-full
+                       bg-White 
+                        w-[10px] 
+                        h-[10px]"
+            ></button>
+            <button
+              type="button"
+              className="rounded-full
+                       bg-White 
+                        w-[10px] 
+                        h-[10px]"
+            ></button>
+            <button
+              type="button"
+              className="rounded-full
+                       bg-White 
+                        w-[10px] 
+                        h-[10px]"
+            ></button>
+          </div>
+          <div 
+            className="font-bellefair  
+                     text-Azul-Claro 
+                       md:w-[65%] 
+                       lg:w-full ">
+            <h3 
+              className="lg:text-3xl"
+            >
+              COMMANDER
+            </h3>
+            <h1 
+              className="text-2xl
+                       text-White 
+                         mt-2 mb-7 
+                         lg:text-[48px]"
+            >
+              DOUGLAS HURLEY
+            </h1>
+            <p 
+              className="font-barlow 
+                         mb-10 
+                         lg:text-xl "
+            >
               Douglas Gerald Hurley is an American engineer, former Marine Corps
               pilot and former NASA astronaut. He launched into space for the
               third time as commander of Crew Dragon Demo-2.
             </p>
-          </div>
-          <img
-            className="h-52 mx-auto md:mx-0 md:mt-6 order-1 md:order-2"
-            src={people}
-            alt=""
-          />
-          <div className="flex gap-4 my-10">
-            <button
-              type="button"
-              className="rounded-full bg-White w-[10px] h-[10px]"
-            ></button>
-            <button
-              type="button"
-              className="rounded-full bg-White w-[10px] h-[10px]"
-            ></button>
-            <button
-              type="button"
-              className="rounded-full bg-White w-[10px] h-[10px]"
-            ></button>
-            <button
-              type="button"
-              className="rounded-full bg-White w-[10px] h-[10px]"
-            ></button>
           </div>
         </div>
       </section>
