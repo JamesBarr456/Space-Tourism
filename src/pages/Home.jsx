47ff2600b01220327f38647cc9bd58f13cbf3e71
Emmanuel Barreto <barretosantiago@hotmail.com>
2023-12-24 15:31:21 -0300

    home casi terminado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,22 +13,24 @@ export const Home = () => {
                    min-h-screen"
       >
         <div
-          className="lg:flex 
-                      md:pt-[100px] 
-                      lg:pt-[160px] 
-                      lg:justify-around">
-          <motion.div
-            initial={{ x: -200, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ duration: 1.5 }}
-            className="flex
+          className="flex 
                      flex-col
-                     lg:w-[450px]
                      items-center
-                     px-6
-                     lg:px-0
-                     pt-28
-                     lg:pt-0"
+                     py-[175px] 
+                     lg:items-end
+                     lg:flex-row
+                     lg:justify-around"
+        >
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 2 }}
+            className="flex
+                       flex-col
+                       text-center
+                       w-[327px]
+                       md:w-[450px]
+                       lg:text-start"
           >
             <h2
               className="font-condensed
@@ -37,55 +39,54 @@ export const Home = () => {
                        lg:text-[28px]
                        tracking-[2.7px]
                        text-Azul-Lavanda
-                       mb-6"
+                      "
             >
               SO, YOU WANT TO TRAVEL TO
             </h2>
             <h1
               className="text-White
                          text-[80px]
-                         md:text-[150px]
-                        font-bellefair
-                        mb-6"
+                         font-bellefair
+                         md:text-[150px]"
             >
               SPACE
             </h1>
             <p
               className="font-barlow 
-                       text-sm
+                       text-base
                        lg:text-lg
                        lg:text-justify
                      text-Azul-Lavanda
-                       text-center
-                       px-4
-                       md:w-[60%]
-                       lg:w-full
+                       px-5
+                       md:px-2
                        lg:px-0
-                       mb-14"
+                       "
             >
               Let’s face it; if you want to go to space, you might as well
-              genuinely go to outer space and not hover kind of on the edge of it.
-              Well sit back, and relax because we’ll give you a truly out of this
-              world experience!
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
             </p>
           </motion.div>
-          <motion.div 
-            initial= {{x:400, opacity:0}}
-            animate={{x:0, opacity:1}}
-            transition={{duration:1.5}}
-            className="flex
-                        justify-center
-                        items-end"
-            >
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 2 }}
+            className="
+                       mt-16
+                       md:mt-28
+                       lg:mt-0"
+          >
             <NavLink
               to={"/destination"}
               className="flex
                         items-center
                         justify-center
+                        w-[150px]
+                        h-[150px]
                       bg-White
                         font-bellefair
                         rounded-full
-                        my-12
                         relative
                         z-[1]
                         hover:after:content-['']
@@ -97,11 +98,12 @@ export const Home = () => {
                         hover:after:h-full
                         hover:after:rounded-full
                         hover:after:scale-150
-                        w-[150px]
-                        h-[150px]
+                        md:w-[242px]
+                        md:h-[242px]
+                        md:text-3xl
                         lg:w-[274px]
                         lg:h-[274px]
-                        lg:text-3xl"
+                        "
             >
               EXPLORE
             </NavLink>
